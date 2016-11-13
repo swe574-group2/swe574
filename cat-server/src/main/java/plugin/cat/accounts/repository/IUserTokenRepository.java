1691f7bfd65b7284982b1fee0d831b859a8c9c80
Okan Menevşeoğlu <okan.menevseoglu@boun.edu.tr>
2016-11-14 00:40:01 +0300

    Accounts is renamed as authentication and is reverted.

--- cat-server/src/main/java/plugin/cat/accounts/repository/IUserTokenRepository.java
+++
@@ -1,7 +0,0 @@
-package plugin.cat.accounts.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import plugin.cat.accounts.model.UserToken;
-
-public interface IUserTokenRepository extends CrudRepository<UserToken, Long> {
-}
