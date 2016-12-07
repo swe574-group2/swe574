b44a782717f5caf4a043aeadb253d0148010617f
Okan Menevseoglu <okanmenevseoglu@gmail.com>
2016-12-07 21:29:01 +0300

    Added new request class.

---
+++ cat-server/src/main/java/plugin/cat/annotation/request/AnnotationRequest.java
@@ -0,0 +1,32 @@
+package plugin.cat.annotation.request;
+
+import lombok.Data;
+
+import java.util.Date;
+
+/**
+ * Created by tolgacaner on 29/10/16.
+ */
+@Data
+public class AnnotationRequest {
+
+    private String targetSource;
+
+    private String nickname;
+
+    private String bodyValue;
+
+    private Date startDate;
+
+    private Date endDate;
+
+    private String selectorType;
+
+    private String motivation;
+
+    boolean isPrivate;
+
+    private int pageNumber;
+
+    private int pageSize;
+}
