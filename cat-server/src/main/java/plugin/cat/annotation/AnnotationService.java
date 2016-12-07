429cb1685a3332d763cc0bfa1c1e7d525fe0eb46
Okan Menevseoglu <okanmenevseoglu@gmail.com>
2016-12-07 21:27:29 +0300

    Removed unnecessary imports and annotations.

--- cat-server/src/main/java/plugin/cat/annotation/AnnotationService.java
+++ cat-server/src/main/java/plugin/cat/annotation/AnnotationService.java
@@ -2,14 +2,10 @@ package plugin.cat.annotation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
-@SpringBootApplication
-@EnableDiscoveryClient
 @EnableEurekaClient
-@EnableWebSecurity
+@SpringBootApplication
 public class AnnotationService {
 
     public static void main(String[] args) {
