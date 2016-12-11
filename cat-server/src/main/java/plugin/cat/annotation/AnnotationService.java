21cd02950ac5d1b3cf52a2544725d8b04ada6f65
Arda Ofluoglu <ardaofluoglu@gmail.com>
2016-12-11 14:56:11 +0300

    Web security annotation added

--- cat-server/src/main/java/plugin/cat/annotation/AnnotationService.java
+++ cat-server/src/main/java/plugin/cat/annotation/AnnotationService.java
@@ -3,9 +3,11 @@ package plugin.cat.annotation;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @EnableEurekaClient
 @SpringBootApplication
+@EnableWebSecurity
 public class AnnotationService {
 
     public static void main(String[] args) {
